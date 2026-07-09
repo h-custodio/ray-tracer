c0f5f0d33c0a6773636ced4045ca46417c399db9
h-custodio <hprince0814@gmail.com>
2026-07-09 13:45:51 -0700

    implemented ray class with at function to find a new point given time

---
+++ src/ray.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "vec3.h"
+
+class ray {
+    private:
+        point3 origin;
+        vec3 direction;
+
+    public:
+        ray();
+        
+};
+
+ray::ray(/* args */) {
+
+}
+
+ray::~ray() {
+
+}
