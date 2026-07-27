26cc86b3718b55d70f55049d47b5194cdc566ada
Hendrick Custodio <hendrickcustodio@Hendricks-MacBook-Air-7.local>
2026-07-26 20:22:09 -0700

    refactored hittable class

--- src/hittable.h
+++ src/hittable.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "ray.h"
+#include <memory>
+#include <vector>
 
 
 class hit_record {
@@ -23,13 +25,40 @@ public:
             normal = -outward_normal;
         }
     }
-
-    
 };
 
 class hittable {
 public:
     virtual ~hittable() = default;
 
-    virtual bool hit(const ray& r, float ray_tmin, float ray_tmax, hit_record& rec) const = 0;
+    virtual bool hit(const ray& r, float ray_tmin, float ray_tmax, hit_record& record) const = 0;
+};
+
+class hittable_list : public hittable {
+    std::vector<std::shared_ptr<hittable>> objects;
+    
+    hittable_list() {};
+    hittable_list(std::shared_ptr<hittable> object) { add(object); } 
+
+    void clear() {
+        objects.clear();
+    }
+
+    void add(std::shared_ptr<hittable> object) {
+        objects.push_back(object);
+    }
+
+    bool hit(const ray& r, float ray_tmin, float ray_tmax, hit_record& record) const override {
+        hit_record temp_record;
+        bool hit_anything = false;
+        auto current_closest = ray_tmax;
+
+        for (const auto& object: objects) {
+            if () {
+                
+            }
+        }
+
+        return hit_anything;
+    }
 };
