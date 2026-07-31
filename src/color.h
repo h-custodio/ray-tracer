ab867da78ef5da72959ba8d7587816c97fb71aa2
Hendrick Custodio <hendrickcustodio@Hendricks-MacBook-Air-7.local>
2026-07-31 10:48:37 -0700

    updated comments to match formatting and clarity

--- src/color.h
+++ src/color.h
@@ -12,12 +12,12 @@ void write_color(std::ostream& out, const color& pixel_color) {
     auto b = pixel_color.z();
 
     // translate the [0,1] component values to the byte range [0,255].
-    // turns into hexadecimal form because P6 is being used
+    // turns into hexadecimal form because PPM P6 is being used
     unsigned char red_byte = static_cast<unsigned char>(255.999 * r);
     unsigned char green_byte = static_cast<unsigned char>(255.999 * g);
     unsigned char blue_byte  = static_cast<unsigned char>(255.999 * b);
 
-    // Write out the pixel color components.
+    // write out the pixel color components.
     out.put(red_byte);
     out.put(green_byte);
     out.put(blue_byte);
