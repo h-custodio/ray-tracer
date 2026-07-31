f813e510a68f4a5d486f767fa2da6fda23d83ce6
Hendrick Custodio <hendrickcustodio@Hendricks-MacBook-Air-7.local>
2026-07-31 14:49:30 -0700

    changed variable names for more clarity

--- src/hittable.h
+++ src/hittable.h
@@ -6,36 +6,34 @@
 #include "ray.h"
 #include "interval.h"
 
-
+// stores information about one hit aka intersection
 class hit_record {
-private:
-
 public:
-    point3 point;
-    vec3 normal;
-    float t_intersect;
-    bool front_face;
+    point3 point;   // point of hit/intersaction
+    vec3 normal;    // surface normal 
+    float ray_position;  // ray position where the hit occured
+    bool front_face;    // is ray from the outside
 
+    // sets the hit_record normal vector
+    // outward_normal is assumed to be normalized when passed
     void set_face_normal(const ray& r, const vec3& outward_normal) {
-        //sets the hit_record normal vector
-        //outward_normal is assumed to be normalized when passed
 
-        front_face = dot_product(r.get_direction(), outward_normal) < 0.0f;
+        front_face = dot_product(r.direction, outward_normal) < 0.0f;
         if (front_face) {
-            normal = outward_normal;    //if ray is from outside of object
+            normal = outward_normal;    // if ray is from outside of object
         } else {
-            normal = -outward_normal;   //if ray is from inside of object
+            normal = -outward_normal;   // if ray is from inside of object
         }
     }
 };
 
-//hittable abstract class to use for objects that can interact with the ray
+// hittable abstract class to use for objects that can interact with the ray
 class hittable {
 public:
     //default destructor
     virtual ~hittable() = default;
 
-    virtual bool hit(const ray& r, interval ray_t_intersection, hit_record& record) const = 0;
+    virtual bool hit(const ray& r, interval rp_intersection, hit_record& record) const = 0;
 };
 
 class hittable_list : public hittable {
@@ -53,16 +51,16 @@ public:
         objects.push_back(object);
     }
 
-    bool hit(const ray& r, interval ray_t_intersection, hit_record& record) const override {
+    bool hit(const ray& r, interval rp_intersection, hit_record& record) const override {
         hit_record temp_record;
         bool hit_anything = false;
-        auto current_closest = ray_t_intersection.get_max();
+        auto current_closest = rp_intersection.get_max();
 
         for (const auto& object: objects) {
             // since object is a shared_ptr, and not an object '->' is used
-            if (object->hit(r, interval(ray_t_intersection.get_min(), current_closest), temp_record)) {
+            if (object->hit(r, interval(rp_intersection.get_min(), current_closest), temp_record)) {
                 hit_anything = true;
-                current_closest = temp_record.t_intersect;
+                current_closest = temp_record.ray_position;
                 record = temp_record;
             }
         }
