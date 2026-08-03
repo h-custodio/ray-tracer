7a9653bd5175a9baf04a696cf20b00877f2f4dc6
h-custodio <hprince0814@gmail.com>
2026-08-03 03:16:49 -0700

    moved default world setup function out of main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
 
 #include "render.h"
 
-void setup_default_world(HittableList& world) {
-    world.add(std::make_shared<Sphere>(Point3(0,0,-1), 0.5));
-    world.add(std::make_shared<Sphere>(Point3(0,-100.5,-1), 100));
-}
-
 int main() {
     HittableList world;
     setup_default_world(world);
--- src/render.h
+++ src/render.h
@@ -81,8 +81,7 @@ public:
     }
 };
 
-
-
-
-
-
+void setup_default_world(HittableList& world) {
+    world.add(std::make_shared<Sphere>(Point3(0,0,-1), 0.5));
+    world.add(std::make_shared<Sphere>(Point3(0,-100.5,-1), 100));
+}
