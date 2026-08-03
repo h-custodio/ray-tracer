dfd963eb646aab69a90ce090d16337f44e43591b
h-custodio <hprince0814@gmail.com>
2026-08-03 16:52:14 -0700

    refactored rendering process to use multiple random sampling

--- src/utils.h
+++ src/utils.h
@@ -4,14 +4,31 @@
 #include <iostream>
 #include <limits>
 #include <numbers>
+#include <random>
 
-// Constants
+// ========== Constants ========== //
 
 const float infinity = std::numeric_limits<float>::infinity();
 const float pi = std::numbers::pi_v<float>;
 
-// Utility Functions
+// ========== Utility Functions ========== //
 
 inline float degrees_to_radians(float degrees) {
     return degrees * pi / 180.0f;
+}
+
+float generate_random() {
+    // static used to initialized once, reused on later calls
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_real_distribution<float> dist(-0.5f, 0.5f); 
+
+    // safeguard to prevent getting a result of 0.5f
+    float random;
+    do {
+        // distribution: [-0.5f, 0.5f)
+        random = dist(gen); 
+    } while (random >= 0.5f);
+
+    return random;
 }
