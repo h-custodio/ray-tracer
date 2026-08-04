eb490a00fc6dfcfc32d85090ba27e5ddb159c80b
h-custodio <hprince0814@gmail.com>
2026-08-04 11:29:22 -0700

    refactored to convert from linear to gamma space aka gamma correction

--- src/color.h
+++ src/color.h
@@ -7,10 +7,23 @@
 
 using Color = Vec3;
 
+inline float linear_to_gamma(float linear_component) {
+    if (linear_component > 0) {
+        return std::sqrt(linear_component);
+    }
+
+    return 0;
+}
+
 void write_color(std::ostream& out, const Color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // Apply a linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     static const Interval intensity(0.000f, 0.999f);
 
